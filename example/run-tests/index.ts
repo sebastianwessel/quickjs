8241a4800eca58415aa07a33063915016b1511ae
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-07-19 23:23:45 +0100

    chore: update examples

--- example/run-tests/index.ts
+++ example/run-tests/index.ts
@@ -1,3 +1,19 @@
+import { quickJS } from '../../src/index.js'
+
+// General setup like loading and init of the QuickJS wasm
+// It is a ressource intensive job and should be done only once if possible
+const { createRuntime } = await quickJS()
+
+// Create a runtime instance each time a js code should be executed
+const { evalCode } = await createRuntime({
+	allowFetch: true, // inject fetch and allow the code to fetch data
+	allowFs: true, // mount a virtual file system and provide node:fs module
+	env: {
+		MY_ENV_VAR: 'env var value',
+	},
+	enableTestUtils: true,
+})
+
 const code = `
 import 'test'
 
@@ -11,3 +27,7 @@ const testResult = await runTests();
 
 export default testResult
 `
+
+const result = await evalCode(code)
+
+console.log(JSON.stringify(result, null, 2))
