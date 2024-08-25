aa7b21a37930ed1679079fed88493ca5eeeb43ca
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-08-25 13:02:01 +0100

    chore: cleanup examples

--- example/typescript/index.ts
+++ example/typescript/index.ts
@@ -5,11 +5,8 @@ import { type SandboxOptions, loadQuickJs } from '../../src/index.js'
 const { runSandboxed } = await loadQuickJs()
 
 const options: SandboxOptions = {
-	allowFetch: true, // inject fetch and allow the code to fetch data
-	allowFs: true, // mount a virtual file system and provide node:fs module
-	env: {
-		MY_ENV_VAR: 'env var value',
-	},
+	// enable typescript transpile
+	// typescript must be installed as peer dependency in the project
 	transformTypescript: true,
 	mountFs: {
 		src: {
@@ -29,4 +26,4 @@ export default { fn: example('Hello World'), custom: custom('Custom string') }
 
 const result = await runSandboxed(async ({ evalCode }) => evalCode(code, undefined, options), options)
 
-console.log(result) // { ok: true, data: '<!doctype html>\n<html>\n[....]</html>\n' }
+console.log(result)
