ebabfa8433a82496e8880232ea6904b4bdef2e48
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2025-06-20 19:57:01 +0100

    enable fs link and permission tests

--- src/test/async/fsModule-links.test.ts
+++ src/test/async/fsModule-links.test.ts
@@ -2,7 +2,7 @@ import { beforeAll, describe, expect, it } from 'bun:test'
 import { loadAsyncQuickJs } from '../../loadAsyncQuickJs.js'
 import type { OkResponse } from '../../types/OkResponse.js'
 
-describe.skip('async - node:fs - links', () => {
+describe('async - node:fs - links', () => {
 	let runtime: Awaited<ReturnType<typeof loadAsyncQuickJs>>
 	const testFilePath = '/test.txt'
 	const testFileContent = 'example content'
